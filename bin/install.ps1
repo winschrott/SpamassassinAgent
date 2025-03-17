6780c846fcf87ac48ceae94d5788b9a6b6d3162a
winschrott <michael.gerstel@ctmd.de>
2025-03-17 15:41:46 +0100

    Update install.ps1

--- bin/install.ps1
+++ bin/install.ps1
@@ -96,23 +96,23 @@ Invoke-Webrequest https://raw.githubusercontent.com/winschrott/SpamassassinAgent
 # Download the XML configuration
 Invoke-WebRequest https://raw.githubusercontent.com/winschrott/SpamassassinAgent/master/etc/SpamassassinConfig.xml -OutFile "C:\CustomAgents\SpamassassinAgentData\SpamassassinConfig.xml"
 
-# Connect to the exchange Server
-. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
-Connect-ExchangeServer -auto
+## Connect to the exchange Server
+#. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
+#Connect-ExchangeServer -auto
 
-# Install the Transport Agent
-Install-TransportAgent -Name "SpamAssassin Agent" -AssemblyPath C:\CustomAgents\SpamassassinAgent.dll -TransportAgentFactory SpamassassinAgent.SpamassassinAgentFactory
-Enable-TransportAgent "Spamassassin Agent"
-#Set-TransportAgent "Spamassassin Agent" -Priority 3
+## Install the Transport Agent
+#Install-TransportAgent -Name "SpamAssassin Agent" -AssemblyPath C:\CustomAgents\SpamassassinAgent.dll -TransportAgentFactory SpamassassinAgent.SpamassassinAgentFactory
+#Enable-TransportAgent "Spamassassin Agent"
+##Set-TransportAgent "Spamassassin Agent" -Priority 3
 
-# Install Anti-Spam Functionality
-#. 'C:\Program Files\Microsoft\Exchange Server\V15\Scripts\Install-AntiSpamAgents.ps1'
+## Install Anti-Spam Functionality
+##. 'C:\Program Files\Microsoft\Exchange Server\V15\Scripts\Install-AntiSpamAgents.ps1'
 
-# Disable Existing Anti-Spam Functionality
-#Set-ContentFilterConfig -Enable $false
-#Set-SenderFilterConfig -Enable $false
-#Set-SenderIDConfig -Enable $false
-#Set-SenderReputationConfig -Enable $false
+## Disable Existing Anti-Spam Functionality
+##Set-ContentFilterConfig -Enable $false
+##Set-SenderFilterConfig -Enable $false
+##Set-SenderIDConfig -Enable $false
+##Set-SenderReputationConfig -Enable $false
 
-# Restart 
-Restart-Service MSExchangeTransport
+## Restart 
+#Restart-Service MSExchangeTransport
